de44d666da135e275349ebbb8b43c96a1a73a324
Andrey Bogoslovskiy <88778576+bojieitsit@users.noreply.github.com>
2023-02-16 18:31:42 +0400

    Calculating BMI

---
+++ BMICalculatorApp/Controllers/ResultViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultViewController.swift
+//  BMICalculatorApp
+//
+//  Created by Andrey on 16.02.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
--- BMICalculatorApp/Controllers/ViewController.swift
+++ BMICalculatorApp/Controllers/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    var height:Double = 1.0
+    var weight:Double = 100
+    var bmiString = ""
+    var bmi = 0.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = "\(String(format: "%.2f", sender.value))m"
+        height = Double(sender.value)
+    }
+    
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = "\(String(format: "%.0f", sender.value))Kg"
+        weight = Double(sender.value)
+    }
+    
+    
+    @IBAction func calculateButton(_ sender: UIButton) {
+        let bmiValue = (weight / pow(height, 2))
+        bmiString = String(format: "%.2f", bmiValue)
+        bmi = Double(bmiString)!
+    }
+    
+    //MARK: NAVIGATION
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        <#code#>
+//    }
+    
+    
 }
 
