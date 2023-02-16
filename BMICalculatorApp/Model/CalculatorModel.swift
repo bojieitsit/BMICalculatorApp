a5eea09401803e8ec136957d8177d44f7dfb3b1c
Andrey Bogoslovskiy <88778576+bojieitsit@users.noreply.github.com>
2023-02-16 19:50:36 +0400

    Advice text changed

--- BMICalculatorApp/Model/CalculatorModel.swift
+++ BMICalculatorApp/Model/CalculatorModel.swift
@@ -15,11 +15,11 @@ struct CalculatorModel {
         let bmiValue = (weight / pow(height, 2))
         
         if bmiValue < 18.5 {
-            bmi = BMI(value: bmiValue, advice: "EAT MORE SNACKS", color: #colorLiteral(red: 0.6965072751, green: 1, blue: 1, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Underweight. You should eat more.", color: #colorLiteral(red: 0.6965072751, green: 1, blue: 1, alpha: 1))
         } else if bmiValue < 24.9 {
-            bmi = BMI(value: bmiValue, advice: "GREAT BMI", color: #colorLiteral(red: 0.7421414256, green: 1, blue: 0.7347304821, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Healthy weight. Keep it up!", color: #colorLiteral(red: 0.7421414256, green: 1, blue: 0.7347304821, alpha: 1))
         } else {
-            bmi = BMI(value: bmiValue, advice: "EAT LESS", color: #colorLiteral(red: 1, green: 0.8139899969, blue: 0.7511866689, alpha: 1)) 
+            bmi = BMI(value: bmiValue, advice: "Overweight. You should eat less.", color: #colorLiteral(red: 1, green: 0.8139899969, blue: 0.7511866689, alpha: 1))
         }
     }
     
