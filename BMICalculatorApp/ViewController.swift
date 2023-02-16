8e9cb8776729ff8f172d6342d0754973f52131fc
Andrey Bogoslovskiy <88778576+bojieitsit@users.noreply.github.com>
2023-02-16 15:39:05 +0400

    Initial Commit

---
+++ BMICalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculatorApp
+//
+//  Created by Andrey on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
